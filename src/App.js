2aba9f7768c1b6d1bf0f018d962f8f8866f33149
chanansiri <nuanpang_c@silpakorn.edu>
2024-03-18 20:36:20 +0700

    input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components'
 
-function App() {
+const StyledInput = styled.input `
+  height : 15px;
+  margin : 10px;
+  width : 90px;
+  text-align: center;
+`
+
+function StarDisplay() {
+  const [numStar, setNumStar] = useState(0);
+  const [stars, setStar] = useState([]);
+
+  const updateStar = () => {
+    const num = parseInt(numStar);
+    const newStar = [];
+    for (let i = 1; i <= num; i++) {
+      newStar.push('*'.repeat(i));
+    }
+    for (let i = num - 1; i > 0; i--) {
+      newStar.push('*'.repeat(i));
+    }
+    setStar(newStar);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Input : 
+        <StyledInput type='num' value={numStar} onChange={(e) => setNumStar(e.target.value)} />
+        <button onClick={updateStar}>Submit</button>
+        {stars.map((star, index) => (
+          <div key={index}>{star}</div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default StarDisplay;
